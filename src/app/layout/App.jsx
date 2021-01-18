34bd1551f995d9d03d55ecec0f2b0e8c44ae6f18
AhmetYasar42 <yasar97.42@live.de>
2021-01-18 14:08:33 +0100

    Footer added

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -3,19 +3,21 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './styles.css'
 import Header from './components/header-components/Header'
+import Footer from './components/footer-components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import TeamScreen from './screens/TeamScreen'
 
 function App() {
-	return (
-		<Router>
-			<Header />
-			<main>
-				<Route path="/" component={HomeScreen} exact />
-				<Route path="/team" component={TeamScreen} exact />
-			</main>
-		</Router>
-	)
+    return (
+        <Router>
+            <Header />
+            <main>
+                <Route path="/" component={HomeScreen} exact />
+                <Route path="/team" component={TeamScreen} exact />
+            </main>
+            <Footer />
+        </Router>
+    )
 }
 
 export default App
