778486263ce3c6e98aeb1a68ef6bc818c3e3432e
nikita-reva <68555738+nikita-reva@users.noreply.github.com>
2021-01-21 20:45:42 +0100

    merged

--- src/app/layout/screens/TeamScreen.jsx
+++ src/app/layout/screens/TeamScreen.jsx
@@ -2,13 +2,34 @@ import React from 'react'
 import ContentContainer from './ContentContainer'
 import ContentSection from './ContentSection'
 import ScreenContainer from './ScreenContainer'
+import './Teamscreen.css'
+import Video from './videos/video1.mp4'
 
 const TeamScreen = () => {
 	return (
 		<ScreenContainer>
-			<ContentContainer background="#1f1faa">
+			<ContentContainer bgi="var(--black-gradient2)">
+				<video className="videoTag" autoPlay muted>
+					<source src={Video} type="video/mp4" />
+				</video>
+
 				<ContentSection>
-					<h1>Team Screen</h1>
+					<div className="Container-title">Unser Trainerteam</div>
+					<div className="Container-main">
+						<div className="Container-info">
+							<div className="Container-info-title">
+								Ajdin Yasar
+							</div>
+							<div className="Container-info-text">
+								Lorem ipsum dolor sit amet consectetur
+								adipisicing elit. Autem optio eveniet
+								voluptatem, ducimus quos incidunt quo, esse
+								labore eos adipisci quae! Tempore natus ea
+								debitis impedit reprehenderit quo perspiciatis
+								facilis?
+							</div>
+						</div>
+					</div>
 				</ContentSection>
 			</ContentContainer>
 		</ScreenContainer>
