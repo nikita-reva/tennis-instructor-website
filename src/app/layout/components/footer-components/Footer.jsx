1a81c383d5b4ad468f66ab46ee411e345f1b99f1
AhmetYasar42 <yasar97.42@live.de>
2021-01-21 20:17:21 +0100

    Team screen added

--- src/app/layout/components/footer-components/Footer.jsx
+++ src/app/layout/components/footer-components/Footer.jsx
@@ -4,72 +4,72 @@ import Sponsor from './images/sponsorRot2.png'
 import TennisClub from './images/stockach.png'
 
 const Footer = () => {
-	return (
-		<>
-			<div className="container-main">
-				<div className="container-1">
-					<ul className="box-1">
-						<b>
-							<li>Kontakt</li>
-						</b>
+    return (
+        <>
+            <div className="container-main">
+                <div className="container-1">
+                    <ul className="box-1">
+                        <b>
+                            <li>Kontakt</li>
+                        </b>
 
-						<li>
-							<i className="fas fa-envelope"></i>&nbsp;&nbsp;
-							<a href="mailto:info@tennis-yasar.de">
-								info@tennis-yasar.de
-							</a>
-						</li>
-						<li>
-							<i className="fas fa-phone-alt"></i>&nbsp;&nbsp;
-							<a href="tel://+4917655217938">+4917655217938</a>
-						</li>
+                        <li>
+                            <i className="fas fa-envelope"></i>&nbsp;&nbsp;
+                            <a href="mailto:info@tennis-yasar.de">
+                                info@tennis-yasar.de
+                            </a>
+                        </li>
+                        <li>
+                            <i className="fas fa-phone-alt"></i>&nbsp;&nbsp;
+                            <a href="tel://+4917655217938">+4917655217938</a>
+                        </li>
 
-						<li>
-							<i className="fas fa-map-marker-alt"></i>
-							&nbsp;&nbsp; Zoznegger Str.3 | 78333 Stockach
-						</li>
-					</ul>
-					<ul className="box-2">
-						<b>
-							<li>Anschrift</li>
-						</b>
-						<li>Tennis Academy Yasar</li>
-						<li>Am Osterholz 4</li>
-						<li>78333 Stockach</li>
-					</ul>
-				</div>
+                        <li>
+                            <i className="fas fa-map-marker-alt"></i>
+                            &nbsp;&nbsp; Zoznegger Str.3 | 78333 Stockach
+                        </li>
+                    </ul>
+                    <ul className="box-2">
+                        <b>
+                            <li>Anschrift</li>
+                        </b>
+                        <li>Tennis Academy Yasar</li>
+                        <li>Am Osterholz 4</li>
+                        <li>78333 Stockach</li>
+                    </ul>
+                </div>
 
-				<div className="container-3">
-					<div className="box-4">
-						<a href="https://www.tecnifibre.com/">
-							<img src={Sponsor} alt="Technifibre" width="180" />
-						</a>
-						<a href="https://www.tc-stockach.de/">
-							<img
-								src={TennisClub}
-								alt="TC-Stockach"
-								width="150"
-							/>
-						</a>
-					</div>
-				</div>
-			</div>
+                <div className="container-3">
+                    <div className="box-4">
+                        <a href="https://www.tecnifibre.com/">
+                            <img src={Sponsor} alt="Technifibre" width="180" />
+                        </a>
+                        <a href="https://www.tc-stockach.de/">
+                            <img
+                                src={TennisClub}
+                                alt="TC-Stockach"
+                                width="150"
+                            />
+                        </a>
+                    </div>
+                </div>
+            </div>
 
-			<div className="container-2">
-				<ul className="box-3">
-					<li>
-						Copyright &copy; {new Date().getFullYear()} Tennis
-						Academy Yasar
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-						Website By Niko Reva & Ahmet Yasar
-						&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Alle
-						Rechte vorbehalten - <b>Imperessum</b> -{' '}
-						<b>Datenschutz</b>
-					</li>
-				</ul>
-			</div>
-		</>
-	)
+            <div className="container-2">
+                <ul className="box-3">
+                    <li>
+                        Copyright &copy; {new Date().getFullYear()} Tennis
+                        Academy Yasar
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        Website By Niko Reva & Ahmet Yasar
+                        &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Alle
+                        Rechte vorbehalten - <b>Imperessum</b> -{' '}
+                        <b>Datenschutz</b>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default Footer
--- src/app/layout/screens/TeamScreen.jsx
+++ src/app/layout/screens/TeamScreen.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
+import './Teamscreen.css'
 import ContentContainer from './ContentContainer'
 import ContentSection from './ContentSection'
 import ScreenContainer from './ScreenContainer'
+import Video from './videos/video1.mp4'
 
 const TeamScreen = () => {
-	return (
-		<ScreenContainer>
-			<ContentContainer>
-				<ContentSection>
-					<h1>Team Screen</h1>
-				</ContentSection>
-			</ContentContainer>
-		</ScreenContainer>
-	)
+    return (
+        <ScreenContainer>
+            <ContentContainer bgi="var(--black-gradient2)">
+                <video className="videoTag" autoPlay muted>
+                    <source src={Video} type="video/mp4" />
+                </video>
+
+                <ContentSection>
+                    <div className="Container-title">Unser Trainerteam</div>
+                    <div className="Container-main">
+                        <div className="Container-info">
+                            <div className="Container-info-title">
+                                Ajdin Yasar
+                            </div>
+                            <div className="Container-info-text">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Autem optio eveniet
+                                voluptatem, ducimus quos incidunt quo, esse
+                                labore eos adipisci quae! Tempore natus ea
+                                debitis impedit reprehenderit quo perspiciatis
+                                facilis?
+                            </div>
+                        </div>
+                    </div>
+                </ContentSection>
+            </ContentContainer>
+        </ScreenContainer>
+    )
 }
 
 export default TeamScreen
