a7fcc5ca0832b4487706f33ee86446e0023bd435
nikita-reva <68555738+nikita-reva@users.noreply.github.com>
2021-01-26 19:02:39 +0100

    New components added

--- src/app/layout/components/events-components/Events.elements.jsx
+++ src/app/layout/components/events-components/Events.elements.jsx
@@ -1,5 +1,22 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 export const EventsContainer = styled.div`
 	width: 100%;
+	display: flex;
+	flex-direction: column;
 `
+
+export const Event = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+`
+
+export const EventDate = styled.h4``
+
+export const EventTitle = styled.h2``
+
+export const EventDescription = styled.p``
+
+export const EventLink = styled(Link)``
