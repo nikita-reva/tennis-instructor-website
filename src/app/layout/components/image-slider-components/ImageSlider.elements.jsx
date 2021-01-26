c99cd19c072849fe2bbfb3e7273a25955707cd13
nikita-reva <68555738+nikita-reva@users.noreply.github.com>
2021-01-26 18:34:14 +0100

    Image slider finished

--- src/app/layout/components/image-slider-components/ImageSlider.elements.jsx
+++ src/app/layout/components/image-slider-components/ImageSlider.elements.jsx
@@ -1,23 +1,37 @@
 import styled, { css } from 'styled-components'
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
 
+export const SliderContainer = styled.div`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`
+
 export const Slider = styled.div`
 	width: 100%;
+	max-width: 960px;
+	margin: 0 auto;
 	height: calc(80vw * (2 / 3));
-	border-radius: 20px;
+	max-height: 640px;
+	border-radius: 15px;
 	overflow: hidden;
 	display: flex;
 	position: relative;
 	justify-content: center;
 	align-items: center;
 	margin: 20px 0;
+
+	@media screen and (max-width: 991px) {
+		height: calc(92vw * (2 / 3));
+	}
 `
 
 const Arrow = css`
 	position: absolute;
 	top: 50%;
 	transform: translateY(-50%);
-	font-size: 3rem;
+	font-size: clamp(2rem, 5vw, 3rem);
 	color: #fff;
 	z-index: 10;
 	transition: opacity 0.4s ease;
@@ -32,12 +46,12 @@ const Arrow = css`
 
 export const LeftArrow = styled(FaArrowAltCircleLeft)`
 	${Arrow}
-	left: 32px;
+	left: clamp(1rem, 5vw, 4rem);
 `
 
 export const RightArrow = styled(FaArrowAltCircleRight)`
 	${Arrow}
-	right: 32px;
+	right: clamp(1rem, 5vw, 4rem);
 `
 
 export const Slide = styled.div`
@@ -51,8 +65,35 @@ export const Slide = styled.div`
 `
 
 export const SliderImage = styled.img`
-	display: block;
 	max-width: 100%;
-	margin: 0;
 	object-fit: cover;
+`
+
+export const SliderNav = styled.div`
+	position: absolute;
+	display: flex;
+	align-items: center;
+	height: 10px;
+	left: 50%;
+	bottom: 5%;
+	transform: translateX(-50%);
+`
+
+export const SliderNavCircle = styled.div`
+	height: 10px;
+	width: 10px;
+	border-radius: 50%;
+	margin: 0 4px;
+	background: #fff;
+	cursor: pointer;
+	opacity: 0.6;
+	transform: translateX(-50%);
+
+	&:hover {
+		opacity: 1;
+	}
+
+	&.active {
+		opacity: 1;
+	}
 `
