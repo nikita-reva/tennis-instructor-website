8aec46ba3b2e4a98c2d953c0cb0e5d0306aac79a
AhmetYasar42 <yasar97.42@live.de>
2021-01-29 21:11:27 +0100

    HomeScreen customized

--- src/app/layout/components/image-slider-components/ImageSlider.elements.jsx
+++ src/app/layout/components/image-slider-components/ImageSlider.elements.jsx
@@ -2,117 +2,117 @@ import styled, { css } from 'styled-components'
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
 
 export const SliderContainer = styled.div`
-	width: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 export const Slider = styled.div`
-	width: 100%;
-	max-width: 960px;
-	margin: 0 auto;
-	height: ${({ hgt }) => (hgt ? hgt : 'calc(80vw * (2 / 3))')};
-	max-height: 640px;
-	border-radius: 15px;
-	overflow: hidden;
-	display: flex;
-	position: relative;
-	justify-content: center;
-	align-items: center;
-	margin: 20px 0;
+    width: 100%;
+    max-width: 960px;
+    margin: 0 auto;
+    height: ${({ hgt }) => (hgt ? hgt : 'calc(80vw * (2 / 3))')};
 
-	@media screen and (max-width: 991px) {
-		height: ${({ hgt }) => (hgt ? hgt : 'calc(92vw * (2 / 3))')};
-	}
+    border-radius: 15px;
+    overflow: hidden;
+    display: flex;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+
+    @media screen and (max-width: 991px) {
+        height: ${({ hgt }) => (hgt ? hgt : 'calc(92vw * (2 / 3))')};
+    }
 `
 
 const Arrow = css`
-	position: absolute;
-	top: 50%;
-	transform: translateY(-50%);
-	font-size: ${({ size }) => (size ? size : 'clamp(2rem, 5vw, 3rem)')};
-	color: #fff;
-	z-index: 10;
-	transition: opacity 0.4s ease;
-	opacity: 0.2;
-	cursor: pointer;
-	user-select: none;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: ${({ size }) => (size ? size : 'clamp(2rem, 5vw, 3rem)')};
+    color: #fff;
+    z-index: 10;
+    transition: opacity 0.4s ease;
+    opacity: 0.2;
+    cursor: pointer;
+    user-select: none;
 
-	&:hover {
-		opacity: 0.8;
-	}
+    &:hover {
+        opacity: 0.8;
+    }
 `
 
 export const LeftArrow = styled(FaArrowAltCircleLeft)`
-	${Arrow}
-	left: ${({ pdl }) => (pdl ? pdl : 'clamp(1rem, 5vw, 4rem)')}; ;
+    ${Arrow}
+    left: ${({ pdl }) => (pdl ? pdl : 'clamp(1rem, 5vw, 4rem)')}; ;
 `
 
 export const RightArrow = styled(FaArrowAltCircleRight)`
-	${Arrow}
-	right: ${({ pdr }) => (pdr ? pdr : 'clamp(1rem, 5vw, 4rem)')}; ;
+    ${Arrow}
+    right: ${({ pdr }) => (pdr ? pdr : 'clamp(1rem, 5vw, 4rem)')}; ;
 `
 
 export const Slide = styled.div`
-	opacity: 0;
-	transition-duration: 1s ease;
-	&.active {
-		opacity: 1;
-		transition-duration: 1s;
-		transform: scale(1.08);
-	}
+    opacity: 0;
+    transition-duration: 1s ease;
+    &.active {
+        opacity: 1;
+        transition-duration: 1s;
+        transform: scale(1.08);
+    }
 `
 
 export const SliderImage = styled.img`
-	max-width: 100%;
-	object-fit: cover;
+    max-width: 100%;
+    object-fit: cover;
 `
 
 export const SliderImageText = styled.p`
-	position: absolute;
-	bottom: 14%;
-	user-select: none;
-	left: 50%;
-	font-size: 12px;
-	color: #fff;
-	background: var(--secondary-color);
-	padding: 4px 8px;
-	border-radius: 12px;
-	transform: translateX(-50%);
+    position: absolute;
+    bottom: 14%;
+    user-select: none;
+    left: 50%;
+    font-size: 12px;
+    color: #fff;
+    background: var(--secondary-color);
+    padding: 4px 8px;
+    border-radius: 12px;
+    transform: translateX(-50%);
 
-	@media screen and (max-width: 600px) {
-		font-size: 8px;
-		padding: 2px 6px;
-		bottom: 16%;
-	}
+    @media screen and (max-width: 600px) {
+        font-size: 8px;
+        padding: 2px 6px;
+        bottom: 16%;
+    }
 `
 
 export const SliderNav = styled.div`
-	position: absolute;
-	display: flex;
-	align-items: center;
-	height: 10px;
-	left: 50%;
-	bottom: 5%;
-	transform: translateX(-50%);
+    position: absolute;
+    display: flex;
+    align-items: center;
+    height: 10px;
+    left: 50%;
+    bottom: 5%;
+    transform: translateX(-50%);
 `
 
 export const SliderNavCircle = styled.div`
-	height: 10px;
-	width: 10px;
-	border-radius: 50%;
-	margin: 0 4px;
-	background: #fff;
-	cursor: pointer;
-	opacity: 0.6;
-	transform: translateX(-50%);
+    height: 10px;
+    width: 10px;
+    border-radius: 50%;
+    margin: 0 4px;
+    background: #fff;
+    cursor: pointer;
+    opacity: 0.6;
+    transform: translateX(-50%);
 
-	&:hover {
-		opacity: 1;
-	}
+    &:hover {
+        opacity: 1;
+    }
 
-	&.active {
-		opacity: 1;
-	}
+    &.active {
+        opacity: 1;
+    }
 `
--- src/app/layout/screens/HomeScreen/HomeScreen.jsx
+++ src/app/layout/screens/HomeScreen/HomeScreen.jsx
@@ -12,88 +12,88 @@ import { HeroHomeScreen, ImageSliderData, EventsData, InfosData } from './Data'
 import { Hero, Events, Infos, ImageSlider } from '../../components'
 
 const Billboard = styled.div`
-	display: flex;
-	flex-direction: column;
-	padding: 20px 0;
+    display: flex;
+    flex-direction: column;
+    padding: 20px 0;
 
-	@media screen and (min-width: 681px) {
-		flex-direction: row;
-		gap: 20px;
-	}
+    @media screen and (min-width: 681px) {
+        flex-direction: row;
+        gap: 20px;
+    }
 `
 
 const BillboardEventsContainer = styled.div`
-	order: 1;
-	margin-bottom: 20px;
+    order: 1;
+    margin-bottom: 20px;
 
-	@media screen and (min-width: 681px) {
-		order: 2;
-		flex-basis: 30%;
-	}
+    @media screen and (min-width: 681px) {
+        order: 2;
+        flex-basis: 30%;
+    }
 
-	@media screen and (min-width: 1101px) {
-		transform: translateY(-18%);
-	}
+    @media screen and (min-width: 1101px) {
+        transform: translateY(-18%);
+    }
 `
 
 const BillboardInfosContainer = styled.div`
-	order: 2;
+    order: 2;
 
-	@media screen and (min-width: 681px) {
-		order: 1;
-		flex-basis: 70%;
-	}
+    @media screen and (min-width: 681px) {
+        order: 1;
+        flex-basis: 70%;
+    }
 `
 
 const ImageSliderContainer = styled.div`
-	position: relative;
-	padding: 100px 0;
+    position: relative;
+    padding: 100px 0;
 `
 
 const HomeScreen = () => {
-	return (
-		<ScreenContainer>
-			<ContentContainer>
-				<Hero {...HeroHomeScreen} />
-			</ContentContainer>
-			<ContentContainer>
-				<ContentSection>
-					<Billboard>
-						<BillboardInfosContainer>
-							<Infos infos={InfosData} />
-						</BillboardInfosContainer>
-						<BillboardEventsContainer>
-							<Events events={EventsData} />
-						</BillboardEventsContainer>
-					</Billboard>
-				</ContentSection>
-			</ContentContainer>
-			<ContentContainer>
-				<ContentSection>
-					<ImageSliderContainer>
-						<ImageSlider
-							slides={ImageSliderData}
-							incrementRate={4000}
-							aspectRatio={3 / 2}
-						/>
-					</ImageSliderContainer>
-				</ContentSection>
-			</ContentContainer>
-			<ContentContainer>
-				<ContainerColorizer bgFill="#000" />
-				<ReactPlayer
-					url={require('../videos/video0.mp4').default}
-					muted={true}
-					loop={true}
-					controls={false}
-					playing={true}
-					playsinline={true}
-					width="100%"
-					height="100%"
-				/>
-			</ContentContainer>
-		</ScreenContainer>
-	)
+    return (
+        <ScreenContainer>
+            <ContentContainer>
+                <Hero {...HeroHomeScreen} />
+            </ContentContainer>
+            <ContentContainer>
+                <ContentSection>
+                    <Billboard>
+                        <BillboardInfosContainer>
+                            <Infos infos={InfosData} />
+                        </BillboardInfosContainer>
+                        <BillboardEventsContainer>
+                            <Events events={EventsData} />
+                        </BillboardEventsContainer>
+                    </Billboard>
+                </ContentSection>
+            </ContentContainer>
+            <ContentContainer>
+                <ContentSection>
+                    <ImageSliderContainer>
+                        <ImageSlider
+                            slides={ImageSliderData}
+                            incrementRate={4000}
+                            aspectRatio={3 / 2}
+                        />
+                    </ImageSliderContainer>
+                </ContentSection>
+            </ContentContainer>
+            <ContentContainer>
+                <ContainerColorizer bgFill="#000" />
+                <ReactPlayer
+                    url={require('../videos/video0.mp4').default}
+                    muted={true}
+                    loop={true}
+                    controls={false}
+                    playing={true}
+                    playsinline={true}
+                    width="100%"
+                    height="100%"
+                />
+            </ContentContainer>
+        </ScreenContainer>
+    )
 }
 
 export default HomeScreen
