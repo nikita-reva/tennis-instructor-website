7aa8653409e8f6c527e8bd4d18d1e63b8749edd2
AhmetYasar42 <yasar97.42@live.de>
2021-02-01 21:42:14 +0100

    Pictures added and Text corrected

--- src/app/layout/screens/HomeScreen/Data.js
+++ src/app/layout/screens/HomeScreen/Data.js
@@ -10,31 +10,6 @@ export const HeroHomeScreen = {
 
 export const ImageSliderData = [
     {
-        image:
-            'https://images.unsplash.com/photo-1545809074-59472b3f5ecc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-        alt: 'Franklin L. Smith',
-    },
-    {
-        image:
-            'https://images.unsplash.com/photo-1554068865-24cecd4e34b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-        alt: 'Brad Goldbaum',
-    },
-    {
-        image:
-            'https://images.unsplash.com/photo-1587280501635-68a0e82cd5ff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-        alt: 'Adam Prett',
-    },
-    {
-        image:
-            'https://images.unsplash.com/photo-1530915365347-e35b749a0381?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-        alt: 'Leroy Jenkins',
-    },
-    {
-        image:
-            'https://images.unsplash.com/photo-1599487436400-6aaf85142f32?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-        alt: 'tennis',
-    },
-    {
         image: require('../images/img1.JPG').default,
         alt: 'Feliciano López',
         text: 'Ajdin & Feliciano López',
@@ -126,7 +101,7 @@ export const InfosData = [
     {
         title: 'Professionelle Tennisausrüstung ',
         text:
-            'In der Tennis Academy Yasar bieten wir Ihnen ein breites Sortiment an professionellem Tennis Equipment. Mit unserem Parnter Tecinifibre haben wir in unserem Sortiment immer die aktuellen Modelle anzubieten. Gerne stehen wir Ihnen bei der richtigen Auswahl beratend zur Seite.',
+            'In der Tennis Academy Yasar bieten wir Ihnen ein breites Sortiment an professionellem Tennis Equipment. Mit unserem Parnter Tecinifibre haben wir in unserem Sortiment immer die aktuellen Modelle anzubieten. Gerne stehen wir Ihnen bei der richtigen Auswahl beratend zur Seite. Zu den Produkten gehören unter anderem: Tennisschläger, Tennistaschen, Tennissaiten, Griffbänder, Tennisbälle, Tennis-Klamotten und vieles mehr!',
         sliderAspectRatio: 1,
         sliderBlackNav: 'true',
         imageSlides: [
@@ -170,6 +145,10 @@ export const InfosData = [
                 image: require('../images/product10.jpg').default,
                 alt: 'tecnifibre',
             },
+            {
+                image: require('../images/schlaeger.jpg').default,
+                alt: 'tecnifibre',
+            },
         ],
         linkTo: '',
         linkText: '',
@@ -178,7 +157,7 @@ export const InfosData = [
         title: 'Ausrüstung & Bespannungsservice',
         text:
             'Die Tennis Academy Yasar bietet sorgfältige Tennisschläger- und Saitenbetreuung und einen profesionellen Bespannungsservice. Wir sind Bespannungspartner von mehreren Profis. Verschiedene Tennissaiten und Griffbänder stehen bei uns zur Auswahl. Bespannungspreis ab 10,- Eur',
-        image: require('../images/saiten.jpg').default,
+        image: require('../images/strings.jpeg').default,
         alt: '',
         linkTo: '/kontakt',
         linkText: 'Service anfragen',
