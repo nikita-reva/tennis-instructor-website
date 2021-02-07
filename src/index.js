3e68287383935dd0c2fea784cc1e7763c8f0c9d9
nikita-reva <68555738+nikita-reva@users.noreply.github.com>
2021-02-07 16:01:16 +0100

    Google Analytics integration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom'
 import App from './app/layout/App'
 import reportWebVitals from './reportWebVitals'
 
+import { createBrowserHistory } from 'history'
+import ReactGA from 'react-ga'
+
+const history = createBrowserHistory()
+ReactGA.initialize('UA-189134177-2')
+history.listen((location) => {
+	ReactGA.set({ page: location.pathname }) // Update the user's current page
+	ReactGA.pageview(location.pathname) // Record a pageview for the given page
+})
+
 const rootElement = document.getElementById('root')
 
 function render() {
